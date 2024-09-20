a64a5a85bab22af774d9cc1456a797d3ee69e28e
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-09-20 12:52:07 -0400

    Se agrego logica para capturar el nombre del producto

--- FRONTEND/src/PAGES/RegisterProduct/COMPONENTS/ProductForm.jsx
+++ FRONTEND/src/PAGES/RegisterProduct/COMPONENTS/ProductForm.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 const ProductForm = () => {
+  const [form, setForm] = useState({
+    name: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value, 
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
             Nombre del Producto <span>*</span>
         </label>
         <input
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          required
         />
         <label>
             Precio <span>*</span>
