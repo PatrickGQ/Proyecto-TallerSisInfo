55c31bfeb9702aa186adb3085e5defff0c261c70
Alejandra Palza Morales <alejandra.palza.morales@gmail.com>
2024-09-25 20:59:22 -0400

    Registrar fecha y hr

--- FRONTEND/src/PAGES/RegisterProduct/COMPONENTS/SeeSales/SalesList.jsx
+++ FRONTEND/src/PAGES/RegisterProduct/COMPONENTS/SeeSales/SalesList.jsx
@@ -5,16 +5,17 @@ const SalesList = ({ sales, setViewSale }) => {
   return (
     <div className="overflow-y-auto h-96 border-t border-b">
       <div className="flex justify-between items-center p-2 bg-gray-200 font-bold">
-        <span className="w-1/3">Cliente</span>
-        <span className="w-1/3">CI</span>
-        <span className="w-1/3">Monto Total</span>
+        <span className="w-1/4">Cliente</span>
+        <span className="w-1/4">CI</span>
+        <span className="w-1/4">Monto Total</span>
+        <span className="w-1/4">Fecha</span>
       </div>
       {sales.length === 0 ? (
         <div className="p-4 text-center text-gray-500">No hay ventas disponibles</div>
       ) : (
         sales.map((sale) => (
           <Link key={sale._id} to={`/sales/seeSales/viewSale/${sale._id}`}>
-            <Sale sale={sale} setViewSale={setViewSale} />
+            <Sale key={sale._id} sale={sale} setViewSale={setViewSale} />
           </Link>
         ))
       )}
