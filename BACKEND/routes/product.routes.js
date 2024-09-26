4efb421f0e4c56592da6ddda6221cf1c3cb8eebf
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-09-26 11:49:40 -0400

    Añadimos las rutas en el backend para realizar el get de los productos

--- BACKEND/routes/product.routes.js
+++ BACKEND/routes/product.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { registerProduct } from "../controllers/products.controller.js";
+import { getProductDB, getProductsDB, registerProduct } from "../controllers/products.controller.js";
 import upload from "../config/multer.config.js";
 
 const productsRouter = Router();
 
 productsRouter.post('/', upload.single('image') , registerProduct);
+productsRouter.get('/', getProductsDB);
+productsRouter.get('/:id', getProductDB);
 
 export default productsRouter;
