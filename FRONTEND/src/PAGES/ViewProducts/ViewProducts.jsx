db40183d689aec923f0077067d20e8675291d865
SalvadorPatty <salvador.patty@ucb.edu.bo>
2024-11-07 16:26:30 -0400

    Se agrego La funcion de receta en detalles de producto

--- FRONTEND/src/PAGES/ViewProducts/ViewProducts.jsx
+++ FRONTEND/src/PAGES/ViewProducts/ViewProducts.jsx
@@ -2,7 +2,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useBranch } from '../../CONTEXTS/BranchContext';
-import { FaEdit, FaShoppingCart, FaTrash } from 'react-icons/fa';
+import { FaBook, FaEdit, FaShoppingCart, FaTrash } from 'react-icons/fa';
 import { CartContext } from '../cart/cartContext';
 import {
   getProductsByBranchRequest,
@@ -199,9 +199,9 @@ const ViewProducts = () => {
             
               <button
                 onClick={(e) => handleEditRecipe(e, product)}
-                className="w-full bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-md transition-colors duration-200 flex items-center justify-center gap-2"
+                className="text-red-500 hover:text-red-700"
               >
-                <FaEdit /> Editar Receta
+                <FaBook size={20} />
               </button>
               <button
                 title="Eliminar producto"
