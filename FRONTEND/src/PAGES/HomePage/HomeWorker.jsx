10341b0765f74979e4ff7f241346be26d4786cb0
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-11-19 20:57:31 -0400

    Se corrigio la pagina home para workers y se hizo responsiba

--- FRONTEND/src/PAGES/HomePage/HomeWorker.jsx
+++ FRONTEND/src/PAGES/HomePage/HomeWorker.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react'; 
-import Slider from 'react-slick';
-import { FaEye, FaBullhorn } from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import Slider from "react-slick";
+import { FaBullhorn } from "react-icons/fa";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useBranch } from '../../CONTEXTS/BranchContext';
-import { getBranchImagesRequest, getBranchTextsRequest } from '../../api/branch';
+import { useBranch } from "../../CONTEXTS/BranchContext";
+import {
+  getBranchImagesRequest,
+  getBranchTextsRequest,
+} from "../../api/branch";
 
 const HomeWorker = () => {
   const [branchMessages, setBranchMessages] = useState([]);
@@ -16,15 +19,15 @@ const HomeWorker = () => {
       if (branchId) {
         // Fetch mensajes de la sucursal
         const messageResponse = await getBranchTextsRequest(branchId);
-        console.log(messageResponse.texts)
+        console.log(messageResponse);
+
         setBranchMessages(messageResponse.texts || []);
-        
-        // Fetch imágenes de la sucursal
+
         const imageResponse = await getBranchImagesRequest(branchId);
         setBranchImages(imageResponse.images || []);
       } else {
-        setBranchMessages([]); 
-        setBranchImages([]); 
+        setBranchMessages([]);
+        setBranchImages([]);
       }
     } catch (error) {
       console.error("Error al obtener los datos de la sucursal:", error);
@@ -33,13 +36,23 @@ const HomeWorker = () => {
 
   useEffect(() => {
     const branchId = selectedBranch
-      ? branches.find(branch => branch.nameBranch === selectedBranch)?._id
+      ? branches.find((branch) => branch.nameBranch === selectedBranch)?._id
       : null;
-    
-    fetchBranchData(branchId);
-  }, [selectedBranch, branches]);  // Reemplazar dos useEffects por uno solo
 
-  // Configuración del carrusel de imágenes motivacionales
+    fetchBranchData(branchId);
+  }, [selectedBranch, branches]);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleString("es-ES", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
   const motivationalSliderSettings = {
     dots: true,
     infinite: true,
@@ -49,9 +62,9 @@ const HomeWorker = () => {
     autoplay: true,
     autoplaySpeed: 3000,
     fade: true,
+    arrows: false
   };
 
-  // Configuración del carrusel de mensajes
   const messagesSliderSettings = {
     dots: true,
     infinite: true,
@@ -61,59 +74,68 @@ const HomeWorker = () => {
     autoplay: true,
     autoplaySpeed: 5000,
     arrows: false,
-    fade: true
+    fade: true,
   };
 
   return (
-    <div className="bg-gray-50 min-h-screen font-sans flex flex-col">
-      {/* Carrusel de Imágenes de la Sucursal */}
-      <section className="my-10">
-        <h2 className="text-center text-3xl font-bold mb-6 text-gray-800">Imágenes de la Sucursal</h2>
-        <Slider {...motivationalSliderSettings} className="w-1/2 mx-auto">
-          {branchImages.map((image, index) => (
-            <div key={index} className="relative transform transition-all hover:scale-105">
-              <img
-                src={`http://localhost:3000/${image.url}`}
-                alt={`Branch image ${index + 1}`}
-                className="w-full h-64 sm:h-96 object-cover rounded-xl shadow-lg"
-              />
-            </div>
-          ))}
-        </Slider>
-      </section>
-
-      {/* Sección de mensajes de la sucursal */}
-      <section className="bg-gradient-to-r from-blue-500 to-teal-500 text-white py-16">
-        <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-6">Mensajes de la Sucursal</h2>
-          <Slider {...messagesSliderSettings} className="max-w-screen-xl mx-auto">
-            {branchMessages.map((message, index) => (
-              <div key={index} className="bg-white text-gray-800 p-8 rounded-lg shadow-2xl transform hover:scale-105 transition-all">
-                <div className="flex items-center justify-center mb-4">
-                  <FaBullhorn className="text-3xl text-yellow-400 mr-2" />
-                </div>
-                <p className="mt-4">{message.content}</p>
+    <>
+      <div className="bg-gray-50 font-sans flex flex-col sm:flex-row">
+        {/* Carrusel de Imágenes de la Sucursal */}
+        <section className="my-10 sm:w-1/2 w-full mx-auto">
+          <h2 className="text-center text-3xl font-bold mb-6 text-gray-800">
+            Comunicados
+          </h2>
+          <Slider {...motivationalSliderSettings} className="w-full">
+            {branchImages.map((image, index) => (
+              <div
+                key={index}
+                className="relative transform transition-all hover:scale-105"
+              >
+                <img
+                  src={`http://localhost:3000/${image.url}`}
+                  alt={`Branch image ${index + 1}`}
+                  className="w-full h-64 sm:h-96 object-cover rounded-xl shadow-lg"
+                />
               </div>
             ))}
           </Slider>
-        </div>
-      </section>
+        </section>
 
-      {/* Botón para ver más mensajes */}
-      <section className="text-center py-8 bg-gray-800">
-        <button
-          onClick={() => alert("Ver más mensajes de la sucursal")}
-          className="bg-yellow-500 text-white py-3 px-8 rounded-lg hover:bg-yellow-600 transition-all flex items-center justify-center mx-auto shadow-md transform hover:scale-105"
-        >
-          <FaEye className="mr-2" /> Ver más mensajes
-        </button>
-      </section>
+        {/* Sección de mensajes de la sucursal */}
+        <section className="bg-gradient-to-r from-blue-500 to-teal-500 text-white py-16 sm:w-1/2 w-full p-2">
+          <div className="container mx-auto text-center">
+            <h2 className="text-3xl font-bold mb-6">Anuncios importantes</h2>
+            <Slider
+              {...messagesSliderSettings}
+              className="w-full max-w-screen-xl mx-auto"
+              key={branchMessages.length} // Recalcular Slider cuando cambien los mensajes
+            >
+              {branchMessages.map((message, index) => (
+                <div
+                  key={index}
+                  className="bg-white text-gray-800 p-8 rounded-lg shadow-2xl transform hover:scale-105 transition-all"
+                >
+                  <div className="flex items-center justify-center mb-4">
+                    <FaBullhorn className="text-3xl text-yellow-400 mr-2" />
+                  </div>
+                  <p className="mt-4">{message.content}</p>
+                  {/* Mostrar la fecha y hora de publicación */}
+                  <p className="mt-4 text-sm text-gray-600">
+                    Publicado el: {formatDate(message.createdAt)}
+                  </p>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </section>
+      </div>
 
-      {/* Estilo de pie de página */}
-      <footer className="text-center py-6 bg-gray-900 text-white">
-        <p className="text-sm">© 2024 - Sistema de Gestión de Sucursal | Todos los derechos reservados</p>
+      <footer className="w-full text-center py-6 bg-gray-900 text-white">
+        <p className="text-sm">
+          © 2024 - Sistema de Gestión de Sucursal | Todos los derechos reservados
+        </p>
       </footer>
-    </div>
+    </>
   );
 };
 
